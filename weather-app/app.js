9277d8f4fbdf0e8a4081edc934b7aded283eba4d
vaclja <rewitablexmetin@gmail.com>
2025-08-11 19:31:11 +0200

    Callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,19 @@
 import geocode from './utils/geocode.js';
 import forecast from './utils/forecast.js';
 
-geocode('Kravaře ve Slezsku, Česká republika', (error, data)=>{
-    console.log('Error', error);
-    console.log('Data', data);
-});
+let city = process.argv[2];
+if(city){
+  geocode(city, (error, data)=>{
+    if(error){
+      return console.log('Error', error);
+    }
+      forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if(error){
+          return console.log('Error', error);
+        }
+        console.log(`It's currently ${forecastData.current.temperature} degrees, ${forecastData.current.weather_descriptions[0]} in ${forecastData.location.name}.`);
+    });
+  });
+}
 
-forecast(49.93317, 18.003372, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-});
+
